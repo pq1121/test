d5f12c9a68a8f011137fec3736a0528da27d0aad
Aleksey <pq1121@mail.ru>
2024-12-15 19:16:11 +0300

    create def multi_1d and change type sum_1d float

--- array_lib.py
+++ array_lib.py
@@ -1,5 +1,11 @@
-def sum_1d(array: list) -> int:
+def sum_1d(array: list) -> float:
     summ = 0
     for i in range(len(array)):
         summ += array[i]
     return summ
+
+def multi_1d(array: list) -> float:
+    multi = 1
+    for i in range(len(array)):
+        multi *= array[i]
+    return multi
