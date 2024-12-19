df9ec9ae6866736764a9a0f9e6063d818baec603
Aleksey <pq1121@mail.ru>
2024-12-19 22:47:48 +0300

    fix(main): update menu and fix small errors

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
 from logger import log_action
 def origin():
+    count = 0
     work = True
     while work:
         print('Выбирите нужное действие')
         print('1. Создание первого массива 1d из случайных чисел')
         print('2. Создание второго массива 1d из случайных чисел')
         print('3. Сумма элементов двух массивов и запись в третий массив')
-        print('4. Выход')
+        print('4. Вывести массив на экран')
+        print('5. Выход')
         num = input()
-        count = 0
         if num == "1":
             from create_array import create_array_1d
             lst1 = create_array_1d()
             count += 1
-            print(log_action("Создание первого массива 1d из случайных чисел"))
-        if num == "2":
+            print(log_action("Создание первого массива 1d из случайных чисел\n"))
+        elif num == "2":
             from create_array import create_array_1d
             lst2 = create_array_1d()
             count += 1
-            print(log_action("Создание второго массива 1d из случайных чисел"))
-        if num == "3" and count == 2:
+            print(log_action("Создание второго массива 1d из случайных чисел\n"))
+        elif num == "3" and count == 2:
             from array_lib import sum_arr
             lst3 = sum_arr(lst1,lst2)
             count = 0
-            print(log_action("суммирование элементов двух массивов"))       
-        if num == "4":
+            print(log_action("суммирование элементов двух массивов\n"))
+        elif num == "4":
+            work1 = True
+            while work1:
+                print('Выбирите нужное действие')
+                print('1. Вывести первый массив')
+                print('2. Вывести второй массив')
+                print('3. Вывести полученный массив')
+                print('4. Выход в предыдущее меню\n')
+                num1 = input()
+                if num1 == "1":
+                    from create_array import output_arr
+                    print(output_arr(lst1))
+                elif num1 == "2":
+                    from create_array import output_arr
+                    print(output_arr(lst2))
+                elif num1 == "3":
+                    from create_array import output_arr
+                    print(output_arr(lst3))
+                elif num1 == "4":
+                    work1 = False
+                else:
+                    print('Не выбран пункт меню!!!')
+        elif num == "5":
             work = False
+        else:
+            print('Не выбран пункт меню!!!')
